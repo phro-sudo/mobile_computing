00748a184c46289890714a09e7344ba8ed03399a
Philiip Rockenschaub <20180045@EDV3MAC01.local>
2021-12-16 13:01:15 +0100

    test

---
+++ NumberGuesser2/NumberGuess/ThreadTableViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ThreadTableViewController.swift
+//  NumberGuess
+//
+//  Created by Philiip Rockenschaub on 16.12.21.
+//
+
+import UIKit
+
+class ThreadTableViewController: UITableViewController {
+    
+    let queue = DispatchQueue(label: "download")
+    let path = "https://jsonplaceholder.typicode.com/todos"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let url = URL(string: path) {
+            queue.async {
+                self.download(url: url)
+            }
+        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    func download(url: URL) {
+        
+        print("downloading \(url)")
+        if let data = try? Data(contentsOf: url) {
+            let obj = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let array = obj as? [[String: Any]] {
+                for todo in array {
+                    if let id = todo["id"] as? Int, let title = todo["title"] as? String {
+                        print("id: \(id), title: \(title)")
+                    }
+                }
+            }
+        } else {
+            print("Kein Parsing")
+        }
+        
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
